13ec9fab5065ea7d63d9ed22e61f0e36c51ebe4a
Garuka404 <garukagimhana@gmail.com>
2024-12-07 23:24:35 +0530

    dded home() method to handle contact management options
    - Displays a user interface with menu options for contact management:
      - Add, Update, Delete, Search, List, and Exit
    - Validates user input to ensure a valid option is selected
    - Implements a loop structure for re-prompting on invalid input

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,43 @@
 public class Main {
+    public static int home(){
+        Scanner scan=new Scanner(System.in);
+        int option;
+        char c=' ',s=' ';
+        do{
+            System.out.println("\n\n\t/$$ /$$$$$$$$ /$$$$$$$  /$$$$$$ /$$$$$$$$ /$$   /$$ /$$$$$$$$");
+            System.out.println("\t|__/| $$_____/| $$__  $$|_  $$_/| $$_____/| $$$ | $$| $$__  $$");
+            System.out.println("\t /$$| $$      | $$  \\ $$  | $$  | $$      | $$$$| $$| $$  \\ $$");
+            System.out.println("\t| $$| $$$$$   | $$$$$$$/  | $$  | $$$$$   | $$ $$ $$| $$  | $$");
+            System.out.println("\t| $$| $$__/   | $$__  $$  | $$  | $$__/   | $$  $$$$| $$  | $$");
+            System.out.println("\t| $$| $$      | $$  \\ $$  | $$  | $$      | $$\\  $$$| $$  | $$");
+            System.out.println("\t| $$| $$      | $$  | $$ /$$$$$$|$$$$$$$$ | $$ \\  $$| $$$$$$$/");
+            System.out.println("\t|__/|__/      |__/  |__/|______/|_______/ |__/  \\__/|_______/");
+            System.out.println("\n\n");
+
+            System.out.println("======================================================================");
+            System.out.println("\n\t[01] ADD Contacts"+
+                    "\n\n\t[02] UPDATE Contacts"+
+                    "\n\n\t[03] DELETE Contacts"+
+                    "\n\n\t[04] SEARCH Contacts"+
+                    "\n\n\t[05] LIST Contacts"+
+                    "\n\n\t[06] EXIT Contacts");
+            do{
+                c=' ';
+                System.out.print("\n\nEnter an option to continue -> ");
+                option=scan.nextInt();
+                if(option<0 || option>7){
+                    System.out.println("\nInvail option number try Again!!!!!!!!!!!!");
+                    c='Y';
+                }
+                else{
+
+                }
+            }while(c=='Y');
+
+        }while(s=='Y');
+
+        return option;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
