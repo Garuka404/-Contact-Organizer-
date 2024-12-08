b228e64963910e2bd1f97c38e5159b497748a086
Garuka404 <garukagimhana@gmail.com>
2024-12-08 23:29:15 +0530

    add contact addition functionality
    - Implemented  method to add new contacts to the list.
    - Added input validations for:
      - Phone number format (must start with '0' and be 10 digits long).
      - Salary (must be a positive number).
    - Enhanced user experience with prompts to re-enter invalid inputs.
    - Successfully stores contact details including name, phone number, company name, salary, and birthday.
    - Includes functionality to add multiple contacts in a single session.

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,79 @@ public class Main {
 
         return option;
     }
+    public static void contact(){
+        Scanner scan=new Scanner(System.in);
+
+        System.out.println("\n\n+---------------------------------------------------+");
+        System.out.println("|            ADD Contact to the list                |");
+        System.out.println("+---------------------------------------------------+");
+        char s= ' ';
+        do{
+            s= ' ';
+            CId=Slen(CId);
+            int x=0000+CId.length;
+            CId[CId.length-1]= "C000"+x;
+            System.out.println("\n\n cid "+CId[CId.length-1]);
+            System.out.println("=======");
+
+            System.out.print("\n Name             :");
+            String name=scan.next();
+
+            CName= Slen(CName);
+            CName[CName.length-1]=name;
+            char c=' ';
+            do{
+                c=' ';
+                System.out.print("\n Phone Number     :");
+                String number=scan.next();
+
+                if(number.length()!=10 ||number.charAt(0)-48!=0){
+
+                    System.out.println("\n\tInvalid phone number");
+                    System.out.print("\n\nDo you want to add phone number again(Y/N):");
+                    c=scan.next().charAt(0);
+                }
+                else{
+                    CPhoneNumber=Slen(CPhoneNumber);
+                    CPhoneNumber[CPhoneNumber.length-1]=number;
+                }
+            }while(c=='Y');
+
+            System.out.print(" Company Name     :");
+            String coname=scan.next();
+            CComnanyname=Slen(CComnanyname);
+            CComnanyname[ CComnanyname.length-1]=coname;
+            do{
+                c=' ';
+                System.out.print(" Salary           :");
+                long salary=scan.nextLong();
+                if(salary>0){
+                    CSalary=Ilen(CSalary);
+                    CSalary[CSalary.length-1]=salary;
+                }
+                else{
+                    System.out.println("\n\tInvalid Salary");
+                    System.out.print("\n\nDo you want to add Salary again(Y/N):");
+                    c=scan.next().charAt(0);
+                }
+            }while(c=='Y');
+
+            System.out.print(" B'Day(YYYY-MM-DD): ");
+            String date=scan.next();
+
+            Cdate= Slen(Cdate);
+            Cdate[Cdate.length-1]=date;
+
+            System.out.println("\nContact has been added successfully...");
+            // System.out.println("Name is "+CName[0]);
+            //  System.out.println("PhoneNumber is "+CPhoneNumber[0]);
+
+            System.out.print("\n\nDo you want to add another Contact(Y/N):");
+            s=scan.next().charAt(0);
+        }while(s=='Y');
+
+        Main2();
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
